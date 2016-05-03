717f29f2cf6f6c3b3bd149e9836fc042f4d5e993
Bryan <bryan@ymail.com>
2016-05-02 21:50:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ module.exports = new Script({
     start: {
         prompt: (bot) => bot.say('I am getting smarter day be day. Please check back with me again.'),
         receive: (bot) => {
-            return bot.say(`Hello! Please choose your language. Bonjour ! Choisissez votre langue, s'il vous plait.\n%[English](postback:BOT) %[Français](postback:francais)`)
-                .then(() => 'askName');
+            return bot.say(`Hello! Please choose your language. Bonjour ! Choisissez votre langue, s'il vous plait.\n%[English](postback:bot) %[Français](postback:francais)`)
+                .then(() => 'speak');
         }
     },
 
