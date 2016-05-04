a042e0a050134f23c75edb9fccd3251abfbd4eeb
Bryan <bryan@ymail.com>
2016-05-03 21:34:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,8 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say(`Select Room \n%[Living Room](postback:livingroom) %[Bedroom](postback:bedroom) %[Dining Room](postback:diningroom) %[More rooms](postback:english)`)
+            return bot.say(`Hello! Please choose your language. Bonjour ! Choisissez votre langue, s'il vous plait.\n%[English](postback:english) %[Français](postback:francais)`)
+            //return bot.say(`Select Room \n%[Living Room](postback:livingroom) %[Bedroom](postback:bedroom) %[Dining Room](postback:diningroom) %[More rooms](postback:english)`)
                 .then(() => 'speak');
         }
     },
